106a60f975d43617933b7ac33e72d93e5ad2c622
molly-edwards <mje8@email.sc.edu>
2026-09-22 14:07:27 -0400

    added logic to else statement

--- day7/script.js
+++ day7/script.js
@@ -7,5 +7,15 @@ function takeDamage() {
     const healthText = document.getElementById("health-display");
     const statusText = document.getElementById("staus-message");
 
-    healthText.innerTExt = playerHealth;
+    if(playerHealth > 0)
+    {
+        healthText.innerText = playerHealth;
+        statusText.innerText = "You've been hit!"
+    }
+    else
+    {
+        healthText.innerText = 0;
+        statusText.innerText = "Game Over!"
+    }
+
 }
